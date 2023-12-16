ce59861c87b3cf2faef1c4e1efaec4efb41db455
mohan dandigam <mohandand@gmail.com>
2023-12-15 19:01:57 -0800

    setup done

--- redux-setup-latest/src/store/store.js
+++ redux-setup-latest/src/store/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import toDoReducer from './Reducers/toDoReducer'
 
 const store = configureStore(
    {
-    reducer: {}
+    reducer: {
+        toDo: toDoReducer
+    }
    }
 )   
 export default store;
